0a83ef062208a74976fd289d05e1c08b63f672c8
Alexandre Barroso <42815351+alexandre-barroso@users.noreply.github.com>
2024-06-13 09:06:15 -0300

    Update analisador.py

--- analisador.py
+++ analisador.py
@@ -9,7 +9,7 @@ def preprocessar_comentario(texto):
     # Remove URLs
     texto = re.sub(r'https?://\S+|www\.\S+', '', texto)
     # Remove pontuações e caracteres especiais
-    texto = re.sub(r'[!\"#$%&\'()*+,\-./:;<=>?@\[\\\]^_`{|}~]+', ' ', texto)
+    texto = re.sub(r'[!\"#$%&\'()*+,\./:;<=>?@\[\\\]^_`{|}~]+', ' ', texto)
     # Remove espaços em excesso
     texto = re.sub(r'\s+', ' ', texto).strip()
     return texto
@@ -119,7 +119,7 @@ categorias = {
     'Contrações': re.compile(r'\b(bora|cum|cuma|cumas|cuns|dum|duma|dumas|duns|fazemo|fizemo|fomo|né|num|numa|numas|nuns|ouvimo|pra|pras|pro|pros|tá|tamo|tava|tô|vamo|vimo|num|numa|numas|numca|pro|pros|pra|pras|ta|to|tou|vamo|vamu|vamu|vimo|vamu)\b', flags=re.IGNORECASE),
     'Gírias': re.compile(r'\b(cringe|cringey|exposed|exp|fake|feke|fak|gado|gad|hype|hyp|lacrar|lacrei|lacrou|lacraste|lacrando|lacra|lacrante|lacração|mitar|mitou|mitando|mito|mitos|ranço|ranços|show|showzaço|showzão|tanka|tankei|tanko|tankaria|tankou|tankando|top|topzera|topzeira|topper|treta|tretas|zoar|zoei|zoa|zoas|zoou|zoando|zoado|zoada|zoeira|role|rolê|roles|rolezinho|chapado|chapada|suave|sussa|susse|paia|pampa|massa|irado|barato|balacobaco|maneiro|daora|da hora|demais|zica|brisa|brisado|brisada|fita|zueira|zueiro|firmeza|firmeza|massa|sussa|belê|belza|véio|véi|véia|véias|mano|manos|mina|minas|migo|miga|brother|brow|sister|sis|cabra|cabrona|bicho|truta|pivete|vacilão|vacilona|vacilou|bolado|bolada|topado|topada|saidêra|zoado|brab|bafômetro|copo sujo|porre|perrengue|trampo|corre|breja|brejas|chop|chopada|bebedeira|manguaça|goró|caixote|caixotado|bico|falsiane|chapisco|serolado|pegada|mandinga|caô|caraio|cascata|cracudo|bafafá|baita|batidão|batida|papagaio|popozuda|ralado|ralada|mala|embatucado|marimba|zé ruela|zigoto|juntado|lombra|lombrar|macumbeiro|mago|batera|miliano|moscando|naba|tá ligado|pá|pego na curva|pé rapado|pelamô|pelego|pindaíba|pistolão|poita|presepada|quebrado|quebra pau|quicar|quitute|ralé|ralo|ramela|ranzinza|resenha|safado|safada|se achar|serumano|seu mano|treta|tiltado|tiltada|tirar onda|tirar um sarro|topado|trampo|tranquilo|vacilão|vaza|vazado|vazando|vaza|vela|zoação|zerado|zica|zicado|zoado|zoação|zoeiro|zona|zoneiro)\b', flags=re.IGNORECASE),
     'Xingamentos': re.compile(r'\b(arrombada|arrombado|babaca|babacas|bosta|bostas|cacete|cacetes|caraio|caraia|caraios|caralho|caralhos|cagão|cagona|cagonas|cagões|canalha|canalhas|cu|cus|cuzão|cuzona|cuzonas|cuzões|escrota|escrotas|escroto|escrotos|estúpida|estúpidas|estúpido|estúpidos|filha da puta|filhas da puta|filho da puta|filhos da puta|foda|foder|fodida|fodidas|fodido|fodidos|idiota|idiotas|imbecil|imbecis|kralho|krl|lixo|lixos|merda|m3rda|merd@|merdas|mongoloide|mongoloides|otária|otárias|otário|otários|pica|picas|pilantra|pilantras|piranha|piranhas|poha|porra|porras|putaria|putarias|retardada|retardadas|retardado|retardados|safada|safadas|safado|safados|trouxa|trouxas|vadia|vadias|vagabunda|vagabundas|vagabundo|vagabundos)\b', flags=re.IGNORECASE),
-    'Abreviações': re.compile(r'\b(adm|adms|admns|amg|amgs|amigo|aq|aqi|aki|bj|bjs|blz|blzs|bo|boa|bom|cap|caps|cm|cms|cmg|cmgs|ctz|ctzs|dms|dmr|dmais|dnv|dnov|doq|dq|dps|dp|dr|drs|f1|fdp|fdps|fds|fimds|fmz|fmzs|flw|flws|fzr|fzrs|glr|glrs|gnt|gnts|hj|hoj|kd|kde|mds|mlk|mlke|mlqs|mlhr|msg|msgs|msm|msms|mt|mts|mto|mta|mtas|mtos|n|nao|nd|nada|ngc|ngm|ningm|nn|non|obg|obgd|oq|oqe|p|pd|pdc|pdcs|pdp|pdps|pft|pfts|pfv|pfvs|plmdds|pmds|pprt|pprts|pq|pqps|psé|pv|pvc|q|qd|qdo|qt|qts|rlx|rlxa|rt|rts|s|sd|sdd|sdds|sla|slc|slk|sm|sma|sqn|ss|t|tb|tbm|td|tds|tlgd|tlgs|tmj|tnc|tnd|trd|trds|tt|tts|vc|vcs|v6|vdd|vdds|vsf|vlw|vtnc|yt|ytb|zap|zaps)\b', flags=re.IGNORECASE),
+    'Abreviações': re.compile(r'\b(adm|adms|admns|amg|amgs|aq|aqi|aki|bj|bjs|blz|blzs|cap|caps|cm|cms|cmg|cmgs|ctz|ctzs|dms|dmr|dmais|dnv|dnov|doq|dq|dps|dp|dr|drs|f1|fdp|fdps|fds|fimds|fmz|fmzs|flw|flws|fzr|fzrs|glr|glrs|gnt|gnts|hj|hoj|kd|kde|mds|mlk|mlke|mlqs|mlhr|msg|msgs|msm|msms|mt|mts|mto|mta|mtas|mtos|n|nao|nd|nada|ngc|ngm|ningm|nn|non|obg|obgd|oq|oqe|p|pd|pdc|pdcs|pdp|pdps|pft|pfts|pfv|pfvs|plmdds|pmds|pprt|pprts|pq|pqps|psé|pv|pvc|q|qd|qdo|qt|qts|rlx|rlxa|rt|rts|s|sd|sdd|sdds|sla|slc|slk|sm|sma|sqn|ss|t|tb|tbm|td|tds|tlgd|tlgs|tmj|tnc|tnd|trd|trds|tt|tts|vc|vcs|v6|vdd|vdds|vsf|vlw|vtnc|yt|ytb|zap|zaps)\b', flags=re.IGNORECASE),
     'Ênclise e Mesóclise': re.compile(r'\b(\w*(?:-a|-á|-ás|-as|-o|-lá|-lo|-los|-na|-nas|-no|-nos|-ia|-ias|-se|-te|-me|-lhe|-lhes|-ei|-emos|-ão|-íamos|-iam|-íeis|-eis|-vos|-os))\b', flags=re.IGNORECASE)
 }
 
@@ -135,11 +135,11 @@ if __name__ == "__main__":
     for arquivo in arquivos_corpus:
         # Calcula o total de palavras no corpus
         total_palavras = 0
-        for df_chunk in pd.read_csv(arquivo, chunksize=5000):
+        for df_chunk in pd.read_csv(arquivo, chunksize=50000):
             total_palavras += df_chunk['Comentario'].apply(contar_palavras).sum()
 
         # Lê cada arquivo em partes (cada parte contendo até 5000 linhas)
-        for df_chunk in pd.read_csv(arquivo, chunksize=5000):
+        for df_chunk in pd.read_csv(arquivo, chunksize=50000):
             # Prepara uma lista de tarefas, onde cada tarefa é uma tupla contendo:
             # (parte dos dados, categoria, padrão regex, nome do arquivo de saída)
             tasks = [(df_chunk, cat, pat, f'{os.path.splitext(arquivo)[0]}_{cat}.csv', total_palavras) for cat, pat in categorias.items()]
